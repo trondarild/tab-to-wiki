9a34bf8992c4fdd1a17ab463dcff22d2fee7fcab
Trond Arild Tjøstheim <trondarild@gmail.com>
2019-05-12 13:18:40 +0200

    added nbsp cleaning

--- tabtowiki.py
+++ tabtowiki.py
@@ -3,6 +3,7 @@
 import re
 import sys
 import os
+import string
 
 # print "got " + str(sys.argv)
 
@@ -15,23 +16,47 @@ fileobj = open(fileandpath, 'r') # open for reading
 lines = fileobj.readlines() # read all lines into array
 fileobj.close()
 
-ptrn = '[\w :;,\."\(\)\-\+\?\–\’\']'
+# clean nobreaking space
+printable = set(string.printable)
+out = []
+for l in lines:
+  a = l.decode("utf-8").replace(u"\xc2", "").replace(u"\xa0", " ").encode("utf-8")
+  #a = filter(lambda x: x in printable, a)
+  out.append(a)
+lines = out
+#lines = map(lambda x: re.sub(u'\xc2', u'', x), lines)
+#lines = map(lambda x: re.sub(u'\xa0', u' ', x), lines)
+
+ptrn = '[\w \:;,\."\(\)\-\+\?\–\’\']'
 p0 = r'<' # allows < as substitute for * bullet points
-p1 = r'^(' +ptrn+ '+)$'
-p2 = r'^\t(' +ptrn+ '+)$'
-p3 = r'^\t\t(' +ptrn+ '+)$'
+p1 = r'^(' +ptrn+ '+)$' # first level heading - part or chapter
+p2 = r'^\t(' +ptrn+ '+)$' # second level heading - chapter or sectinon
+p3 = r'^\t\t(' +ptrn+ '+)$' # third level section or subsection
+p31 = r'^\t\t\t(' +ptrn+ '+)$'
+
 p4 = r'\t' # allows bullet points at same tab level in source file
 p5 = r'§(.+)$' # allows tabbed sections of text from reports etc
 
-dict = {p0:'*', p1:"===\\1===", p2:"====\\1====", p3:"\n'''\\1'''"}#, p4:''}
+leveldict = {p0:'*', p1:"===\\1===", p2:"====\\1====", p3:"=====\\1=====", p31:"\n'''\\1'''"}#, p4:''}
 
 
 lst = lines
-for key in dict.keys():
-  lst = map(lambda x: re.sub(key, dict[key], x), lst)
+for key in leveldict.keys():
+  lst = map(lambda x: re.sub(key, leveldict[key], x), lst)
 lst = map(lambda x: re.sub(p4, '', x), lst)
 lst = map(lambda x: re.sub(p5, '\\1\n', x), lst)
 print ''.join(lst)
+
+##debug
+'''
+outlines = []
+for l in lines:
+  subs = l
+  for key, val in leveldict.iteritems():
+    subs = re.sub(key, val, subs)
+  outlines.append(subs) 
+print "".join(outlines)
+'''
 
 '''
 def repltab(str):
